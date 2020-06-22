659413e6fa2fbeef6c7ea88671fc209121644b9c
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-22 23:58:36 +0630

    Update index.ts

--- src/models/internals/index.ts
+++ src/models/internals/index.ts
@@ -1,3 +1,4 @@
+export * from './build-command-options';
 export * from './build-options-internal';
 export * from './global-script-style-parsed-entry';
 export * from './lib-bundle-options-internal';
