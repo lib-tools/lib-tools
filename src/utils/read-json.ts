46fa353d7d9f2bc830a4bcdc83c7564877ff3245
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-22 21:49:17 +0630

    Update read-json.ts

--- src/utils/read-json.ts
+++ src/utils/read-json.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { readFile } from 'fs';
 
 import { stripComments } from './strip-comments';
