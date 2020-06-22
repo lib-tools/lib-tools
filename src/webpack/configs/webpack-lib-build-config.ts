86b215b9d825aec152736155a3de1cbc9e056405
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-22 23:59:10 +0630

    Add webpack-lib-build-config.ts

---
+++ src/webpack/configs/webpack-lib-build-config.ts
@@ -0,0 +1,25 @@
+import { pathExists } from 'fs-extra';
+import { Configuration } from 'webpack';
+
+import { InvalidConfigError } from '../../models/errors';
+import { BuildCommandOptions } from '../../models/internals';
+
+export async function getWebpackLibBuildConfig(
+    configPath: string,
+    env?: string | { [key: string]: boolean | string },
+    argv?: BuildCommandOptions,
+    logger?: Logger
+): Promise<Configuration> {
+    const startTime = argv && argv._startTime && typeof argv._startTime === 'number' ? argv._startTime : Date.now();
+    if (!configPath || !configPath.length) {
+        throw new InvalidConfigError("The 'configPath' is required.");
+    }
+
+    if (!/\.json$/i.test(configPath)) {
+        throw new InvalidConfigError(`Invalid config file, path: ${configPath}.`);
+    }
+
+    if (!(await pathExists(configPath))) {
+        throw new InvalidConfigError(`The config file does not exist at ${configPath}.`);
+    }
+}
