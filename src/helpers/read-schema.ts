94898e9122cac6c66b8e8adcda85314a9a41d5a9
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-23 15:13:51 +0630

    Update read-schema.ts

--- src/helpers/read-schema.ts
+++ src/helpers/read-schema.ts
@@ -18,3 +18,20 @@ export async function readSchema(): Promise<{ [key: string]: unknown }> {
 
     return schema;
 }
+
+export async function readProjectConfigSchema(): Promise<{ [key: string]: unknown }> {
+    if (((global as unknown) as { [key: string]: unknown }).libConfigSchema) {
+        return ((global as unknown) as { [key: string]: unknown }).libConfigSchema as { [key: string]: unknown };
+    }
+
+    const schemaRootPath = path.resolve(__dirname, '../schemas');
+    const schema = await readJson(path.resolve(schemaRootPath, 'project-config-schema.json'));
+
+    if (schema.$schema) {
+        delete schema.$schema;
+    }
+
+    ((global as unknown) as { [key: string]: unknown }).libConfigSchema = schema;
+
+    return schema;
+}
