947c710d5c89147676ba3cf45ef44a8cf503f5be
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-26 16:58:34 +0630

    Update validate-output-path.ts

--- src/helpers/validate-output-path.ts
+++ src/helpers/validate-output-path.ts
@@ -1,68 +1,52 @@
 import * as path from 'path';
 
 import { InvalidConfigError } from '../models/errors';
-import { LibProjectConfigInternal } from '../models/internals';
 import { isInFolder, isSamePaths } from '../utils';
 
-export function validateOutputPath(workspaceRoot: string, projectConfig: LibProjectConfigInternal): void {
-    if (!projectConfig.outputPath) {
-        throw new InvalidConfigError(
-            `The 'projects[${projectConfig.name || projectConfig._index}].outputPath' is required.`
-        );
+export function validateOutputPath(
+    workspaceRoot: string,
+    projectRoot: string,
+    outputPath: string,
+    projectIndexName: string
+): void {
+    if (path.isAbsolute(outputPath)) {
+        throw new InvalidConfigError(`The 'projects[${projectIndexName}].outputPath' must be relative path.`);
     }
-
-    if (path.isAbsolute(projectConfig.outputPath)) {
-        throw new InvalidConfigError(
-            `The 'projects[${projectConfig.name || projectConfig._index}].outputPath' must be relative path.`
-        );
-    }
-
-    const projectRoot = path.resolve(workspaceRoot, projectConfig.root || '');
-
-    const outputPath = path.resolve(workspaceRoot, projectConfig.outputPath);
 
     if (isSamePaths(workspaceRoot, outputPath)) {
         throw new InvalidConfigError(
-            `The 'projects[${
-                projectConfig.name || projectConfig._index
-            }].outputPath' must not be the same as workspace root directory.`
+            `The 'projects[${projectIndexName}].outputPath' must not be the same as workspace root directory.`
         );
     }
+
     if (isSamePaths(projectRoot, outputPath) || outputPath === '.') {
         throw new InvalidConfigError(
-            `The 'projects[${
-                projectConfig.name || projectConfig._index
-            }].outputPath' must not be the same as project root directory.`
+            `The 'projects[${projectIndexName}].outputPath' must not be the same as project root directory.`
         );
     }
+
     if (outputPath === path.parse(outputPath).root) {
         throw new InvalidConfigError(
-            `The 'projects[${
-                projectConfig.name || projectConfig._index
-            }].outputPath' must not be the same as system root directory.`
+            `The 'projects[${projectIndexName}].outputPath' must not be the same as system root directory.`
         );
     }
 
     const srcDirHomeRoot = path.parse(projectRoot).root;
     if (outputPath === srcDirHomeRoot) {
         throw new InvalidConfigError(
-            `The 'projects[${
-                projectConfig.name || projectConfig._index
-            }].outputPath' must not be the same as system root directory.`
+            `The 'projects[${projectIndexName}].outputPath' must not be the same as system root directory.`
         );
     }
+
     if (isInFolder(outputPath, workspaceRoot)) {
         throw new InvalidConfigError(
-            `The workspace folder must not be inside 'projects[${
-                projectConfig.name || projectConfig._index
-            }].outputPath' directory.`
+            `The workspace folder must not be inside 'projects[${projectIndexName}].outputPath' directory.`
         );
     }
+
     if (isInFolder(outputPath, projectRoot)) {
         throw new InvalidConfigError(
-            `The project root folder must not be inside 'projects[${
-                projectConfig.name || projectConfig._index
-            }].outputPath' directory.`
+            `The project root folder must not be inside 'projects[${projectIndexName}].outputPath' directory.`
         );
     }
 }
