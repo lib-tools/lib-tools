b1b8cbd93fb9776fefb6e7ab3935f4ed4280e23f
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-27 14:57:52 +0630

    Add get-node-modules-path.ts

---
+++ src/helpers/get-node-modules-path.ts
@@ -0,0 +1,22 @@
+import * as path from 'path';
+import { findUp } from '../utils';
+
+const cache: { nodeModulesPath: string | null } = {
+    nodeModulesPath: null
+};
+
+export async function getNodeModulesPath(workspaceRoot: string): Promise<string | null> {
+    if (cache.nodeModulesPath != null) {
+        return cache.nodeModulesPath ? cache.nodeModulesPath : null;
+    }
+
+    const foundNodeModulesPath = await findUp('node_modules', workspaceRoot, path.parse(workspaceRoot).root);
+
+    if (foundNodeModulesPath) {
+        cache.nodeModulesPath = foundNodeModulesPath;
+    } else {
+        cache.nodeModulesPath = '';
+    }
+
+    return foundNodeModulesPath;
+}
