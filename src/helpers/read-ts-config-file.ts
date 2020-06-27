1490c6974b19290c78bc553db9ce5b9ec23c7700
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-27 17:19:38 +0630

    Add read-ts-config-file.ts

---
+++ src/helpers/read-ts-config-file.ts
@@ -0,0 +1,26 @@
+import * as ts from 'typescript';
+
+import { InvalidConfigError } from '../models/errors';
+import { formatTsDiagnostics } from '../utils';
+
+const tsConfigJsonMap = new Map<string, { [key: string]: unknown }>();
+
+export function readTsConfigFile(tsConfigPath: string): { [key: string]: unknown } {
+    const cachedTsConfigJson = tsConfigJsonMap.get(tsConfigPath);
+    if (cachedTsConfigJson) {
+        return cachedTsConfigJson;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    const jsonConfigFile = ts.readConfigFile(tsConfigPath, ts.sys.readFile);
+    if (jsonConfigFile.error && jsonConfigFile.error.length) {
+        const formattedMsg = formatTsDiagnostics(jsonConfigFile.error);
+        if (formattedMsg) {
+            throw new InvalidConfigError(formattedMsg);
+        }
+    }
+
+    tsConfigJsonMap.set(tsConfigPath, jsonConfigFile.config);
+
+    return jsonConfigFile.config;
+}
