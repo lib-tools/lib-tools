70144d4502559999f49d0590c9c46d5729689db0
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-28 17:04:02 +0630

    Update build-options-internal.ts

--- src/models/internals/build-options-internal.ts
+++ src/models/internals/build-options-internal.ts
@@ -1,16 +1,5 @@
-import { BuildOptions, ShortcutBuildOptions } from '../build-options';
+import { BuildOptions } from '../build-options';
 
 export interface BuildOptionsInternal extends BuildOptions {
     environment: { [key: string]: boolean | string };
-}
-
-export interface BuildCommandOptions extends BuildOptions, ShortcutBuildOptions {
-    config?: string;
-
-    _startTime?: number;
-    _fromBuiltInCli?: boolean;
-    _cliIsGlobal?: boolean;
-    _cliIsLink?: boolean;
-    _cliRootPath?: string;
-    _cliVersion?: string;
 }
