b60b89476b44e51928d1fdcccfd00bd377ee918e
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-30 00:21:06 +0630

    Update lib-config-internal.ts

--- src/models/internals/lib-config-internal.ts
+++ src/models/internals/lib-config-internal.ts
@@ -3,6 +3,8 @@ import { LibConfig } from '../lib-config';
 import { ProjectConfigInternal } from './project-config-internal';
 
 export interface LibConfigInternal extends LibConfig {
-    projects: ProjectConfigInternal[];
+    projects: {
+        [key: string]: ProjectConfigInternal;
+    };
     _configPath: string;
 }
