5ebc8ba07e0b76b6712b86e35e894b95678bd72c
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-30 00:20:31 +0630

    Update project-config-internal.ts

--- src/models/internals/project-config-internal.ts
+++ src/models/internals/project-config-internal.ts
@@ -1,7 +1,5 @@
 import { ProjectConfig } from '../project-config';
 
 export interface ProjectConfigInternal extends ProjectConfig {
-    _index: number;
     _configPath: string;
-    _workspaceRoot: string;
 }
