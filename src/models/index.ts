60c7ac4e46287522ef030f2cf00ec578c586b82d
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-30 13:52:41 +0630

    Update models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,5 @@
 export * from './build-command-options';
 export * from './build-options';
 export * from './lib-config';
+export * from './project-build-config';
 export * from './project-config';
-export * from './project-config-build';
