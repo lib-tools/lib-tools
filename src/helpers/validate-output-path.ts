c1ba262190d67d94115970d3994482ab609f1946
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-30 19:31:06 +0630

    Update validate-output-path.ts

--- src/helpers/validate-output-path.ts
+++ src/helpers/validate-output-path.ts
@@ -7,46 +7,63 @@ export function validateOutputPath(
     outputPath: string,
     workspaceRoot: string,
     projectRoot: string,
-    projectIndexName: string
+    configPath: string,
+    configErrorLocation: string
 ): void {
     if (path.isAbsolute(outputPath)) {
-        throw new InvalidConfigError(`The 'projects[${projectIndexName}].outputPath' must be relative path.`);
+        throw new InvalidConfigError(
+            `The '${configErrorLocation}' must be relative path.`,
+            configPath,
+            configErrorLocation
+        );
     }
 
     if (isSamePaths(workspaceRoot, outputPath)) {
         throw new InvalidConfigError(
-            `The 'projects[${projectIndexName}].outputPath' must not be the same as workspace root directory.`
+            `The '${configErrorLocation}' must not be the same as workspace root directory.`,
+            configPath,
+            configErrorLocation
         );
     }
 
     if (isSamePaths(projectRoot, outputPath) || outputPath === '.') {
         throw new InvalidConfigError(
-            `The 'projects[${projectIndexName}].outputPath' must not be the same as project root directory.`
+            `The '${configErrorLocation}' must not be the same as project root directory.`,
+            configPath,
+            configErrorLocation
         );
     }
 
     if (outputPath === path.parse(outputPath).root) {
         throw new InvalidConfigError(
-            `The 'projects[${projectIndexName}].outputPath' must not be the same as system root directory.`
+            `The '${configErrorLocation}' must not be the same as system root directory.`,
+            configPath,
+            configErrorLocation
         );
     }
 
     const srcDirHomeRoot = path.parse(projectRoot).root;
     if (outputPath === srcDirHomeRoot) {
         throw new InvalidConfigError(
-            `The 'projects[${projectIndexName}].outputPath' must not be the same as system root directory.`
+            `The '${configErrorLocation}' must not be the same as system root directory.`,
+            configPath,
+            configErrorLocation
         );
     }
 
     if (isInFolder(outputPath, workspaceRoot)) {
         throw new InvalidConfigError(
-            `The workspace folder must not be inside 'projects[${projectIndexName}].outputPath' directory.`
+            `The workspace folder must not be inside '${configErrorLocation}' directory.`,
+            configPath,
+            configErrorLocation
         );
     }
 
     if (isInFolder(outputPath, projectRoot)) {
         throw new InvalidConfigError(
-            `The project root folder must not be inside 'projects[${projectIndexName}].outputPath' directory.`
+            `The project root folder must not be inside '${configErrorLocation}' directory.`,
+            configPath,
+            configErrorLocation
         );
     }
 }
