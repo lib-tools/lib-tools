0d44d9637f23b684a0e4488d23b65f6650fb1ae3
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-30 22:12:34 +0630

    Update read-ts-config-file.ts

--- src/helpers/read-ts-config-file.ts
+++ src/helpers/read-ts-config-file.ts
@@ -1,15 +1,10 @@
 import * as ts from 'typescript';
 
-import { InvalidConfigError } from '../models/errors';
 import { formatTsDiagnostics } from '../utils';
 
 const tsConfigJsonMap = new Map<string, { [key: string]: unknown }>();
 
-export function readTsConfigFile(
-    tsConfigPath: string,
-    configPath: string,
-    configErrorLocation: string
-): { [key: string]: unknown } {
+export function readTsConfigFile(tsConfigPath: string): { [key: string]: unknown } {
     const cachedTsConfigJson = tsConfigJsonMap.get(tsConfigPath);
     if (cachedTsConfigJson) {
         return cachedTsConfigJson;
@@ -18,10 +13,9 @@ export function readTsConfigFile(
     // eslint-disable-next-line @typescript-eslint/unbound-method
     const jsonConfigFile = ts.readConfigFile(tsConfigPath, ts.sys.readFile);
     if (jsonConfigFile.error && jsonConfigFile.error.length) {
-        let formattedMsg = formatTsDiagnostics(jsonConfigFile.error);
+        const formattedMsg = formatTsDiagnostics(jsonConfigFile.error);
         if (formattedMsg) {
-            formattedMsg += `\nConfig location ${configErrorLocation}.`;
-            throw new InvalidConfigError(formattedMsg, configPath, configErrorLocation);
+            throw new Error(formattedMsg);
         }
     }
 
