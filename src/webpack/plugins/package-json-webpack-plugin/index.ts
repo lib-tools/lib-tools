fa2c4aa86b8c2300fd36564420b61205c9b98312
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-03 11:02:32 +0630

    Update package-json-webpack-plugin

--- src/webpack/plugins/package-json-webpack-plugin/index.ts
+++ src/webpack/plugins/package-json-webpack-plugin/index.ts
@@ -1,9 +1,13 @@
+import * as path from 'path';
+
+import { ensureDir, writeFile } from 'fs-extra';
 import * as webpack from 'webpack';
 
-import { ProjectBuildConfigInternal } from '../../../models/internals';
+import { PackageJsonLike, ProjectBuildConfigInternal } from '../../../models/internals';
 import { LogLevelString, Logger } from '../../../utils';
 
-import { copyPackageJsonFile } from './copy-package-json-file';
+const placeholderRegExp = /\[PLACEHOLDER\]/;
+const versionPlaceholderRegex = new RegExp('0.0.0-PLACEHOLDER');
 
 export interface PackageJsonFileWebpackPluginOptions {
     projectBuildConfig: ProjectBuildConfigInternal;
@@ -12,6 +16,7 @@ export interface PackageJsonFileWebpackPluginOptions {
 
 export class PackageJsonFileWebpackPlugin {
     private readonly logger: Logger;
+    private readonly projectBuildConfig: ProjectBuildConfigInternal;
 
     get name(): string {
         return 'package-json-webpack-plugin';
@@ -23,11 +28,146 @@ export class PackageJsonFileWebpackPlugin {
             debugPrefix: `[${this.name}]`,
             infoPrefix: ''
         });
+        this.projectBuildConfig = this.options.projectBuildConfig;
     }
 
     apply(compiler: webpack.Compiler): void {
-        compiler.hooks.emit.tapPromise(this.name, async () =>
-            copyPackageJsonFile(this.options.projectBuildConfig, this.logger)
+        compiler.hooks.emit.tapPromise(this.name, async () => this.copyPackageJsonFile());
+    }
+
+    async copyPackageJsonFile(): Promise<void> {
+        if (!this.projectBuildConfig._shouldCopyPackageJson) {
+            return;
+        }
+
+        this.logger.info('Copying and updating package.json file');
+
+        // merge config
+        const packageJson: PackageJsonLike = {
+            ...JSON.parse(JSON.stringify(this.projectBuildConfig._packageJson)),
+            ...(this.projectBuildConfig._packageEntryPoints || {})
+        };
+
+        const rootPackageJson = this.projectBuildConfig._rootPackageJson;
+        const packageJsonPathAreEqual =
+            this.projectBuildConfig._rootPackageJsonPath === this.projectBuildConfig._packageJsonPath;
+        const nestedPackage = this.projectBuildConfig._nestedPackage;
+
+        if (packageJson.devDependencies) {
+            delete packageJson.devDependencies;
+        }
+
+        if (rootPackageJson && !packageJsonPathAreEqual) {
+            if (
+                rootPackageJson.description &&
+                (packageJson.description === '' ||
+                    packageJson.description === '[PLACEHOLDER]' ||
+                    (!nestedPackage && !packageJson.description))
+            ) {
+                packageJson.description = rootPackageJson.description;
+            }
+
+            if (
+                rootPackageJson.keywords &&
+                rootPackageJson.keywords.length &&
+                ((packageJson.keywords && !packageJson.keywords.length) || (!nestedPackage && !packageJson.keywords))
+            ) {
+                packageJson.keywords = [...rootPackageJson.keywords];
+            }
+
+            if (
+                rootPackageJson.author &&
+                (packageJson.author === '' ||
+                    packageJson.author === '[PLACEHOLDER]' ||
+                    (!nestedPackage && !packageJson.author))
+            ) {
+                packageJson.author =
+                    typeof rootPackageJson.author === 'string' ? rootPackageJson.author : { ...rootPackageJson.author };
+            }
+
+            if (
+                rootPackageJson.license &&
+                (packageJson.license === '' ||
+                    packageJson.license === '[PLACEHOLDER]' ||
+                    (!nestedPackage && !packageJson.license))
+            ) {
+                packageJson.license = rootPackageJson.license;
+            }
+
+            if (
+                rootPackageJson.repository &&
+                (packageJson.repository === '' ||
+                    packageJson.repository === '[PLACEHOLDER]' ||
+                    (!nestedPackage && !packageJson.repository))
+            ) {
+                packageJson.repository =
+                    typeof rootPackageJson.repository === 'string'
+                        ? rootPackageJson.repository
+                        : { ...rootPackageJson.repository };
+            }
+
+            if (
+                rootPackageJson.bugs &&
+                (packageJson.bugs === '' ||
+                    packageJson.bugs === '[PLACEHOLDER]' ||
+                    (!nestedPackage && !packageJson.bugs))
+            ) {
+                packageJson.bugs =
+                    typeof rootPackageJson.bugs === 'string' ? rootPackageJson.bugs : { ...rootPackageJson.bugs };
+            }
+
+            if (
+                rootPackageJson.homepage &&
+                (packageJson.homepage === '' ||
+                    packageJson.homepage === '[PLACEHOLDER]' ||
+                    (!nestedPackage && !packageJson.homepage))
+            ) {
+                packageJson.homepage = rootPackageJson.homepage;
+            }
+        }
+
+        if (
+            packageJson.sideEffects == null &&
+            ((this.projectBuildConfig._tsTranspilations && this.projectBuildConfig._tsTranspilations.length) ||
+                (this.projectBuildConfig._bundles && this.projectBuildConfig._bundles.length))
+        ) {
+            packageJson.sideEffects = false;
+        }
+
+        if (!packageJson.version) {
+            packageJson.version = this.projectBuildConfig._packageVersion;
+        }
+
+        if (versionPlaceholderRegex.test(packageJson.version) || placeholderRegExp.test(packageJson.version)) {
+            packageJson.version = this.projectBuildConfig._packageVersion;
+        }
+
+        if (packageJson.peerDependencies) {
+            const peerDependencies = packageJson.peerDependencies;
+            const peerKeys = Object.keys(peerDependencies);
+            for (const key of peerKeys) {
+                const peerPkgVer = peerDependencies[key];
+                if (versionPlaceholderRegex.test(peerPkgVer)) {
+                    peerDependencies[key] = peerPkgVer.replace(
+                        versionPlaceholderRegex,
+                        this.projectBuildConfig._packageVersion
+                    );
+                } else if (placeholderRegExp.test(peerPkgVer)) {
+                    peerDependencies[key] = peerPkgVer.replace(
+                        placeholderRegExp,
+                        this.projectBuildConfig._packageVersion
+                    );
+                }
+            }
+
+            packageJson.peerDependencies = peerDependencies;
+        }
+
+        // write package config
+        await ensureDir(this.projectBuildConfig._packageJsonOutDir);
+        await writeFile(
+            path.resolve(this.projectBuildConfig._packageJsonOutDir, 'package.json'),
+            JSON.stringify(packageJson, null, 2)
         );
     }
 }
