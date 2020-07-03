077cc97ea99fa366d08da057ea4bb156ce1ca7d3
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-03 12:25:47 +0630

    Update package-jon-like.ts

--- src/models/internals/package-jon-like.ts
+++ src/models/internals/package-jon-like.ts
@@ -1,4 +1,31 @@
-export interface PackageEntrypoints {
+export interface PackageJsonPredefinedScripts {
+    [key: string]: string | undefined;
+    prepublish?: string;
+    prepare?: string;
+    prepublishOnly?: string;
+    prepack?: string;
+    postpack?: string;
+    publish?: string;
+    preinstall?: string;
+    install?: string;
+    preuninstall?: string;
+    postuninstall?: string;
+    preversion?: string;
+    version?: string;
+    postversion?: string;
+    preshrinkwrap?: string;
+}
+
+export interface PackageJsonLike {
+    [key: string]: string | boolean | { [key: string]: string | undefined } | string[] | undefined;
+    name: string;
+    version?: string;
+    author?: string | { [key: string]: string };
+    keywords?: string[];
+    license?: string;
+    homepage?: string;
+    repository?: string | { [key: string]: string };
+    bugs?: string | { [key: string]: string };
     main?: string;
     module?: string;
     es2015?: string;
@@ -9,22 +36,9 @@ export interface PackageEntrypoints {
     fesm5?: string;
     typings?: string;
     browser?: string;
-}
-
-export interface PackageJsonLike extends PackageEntrypoints {
-    [key: string]: string | boolean | { [key: string]: string } | string[] | undefined;
-    name: string;
-    version?: string;
-    author?: string | { [key: string]: string };
-    keywords?: string[];
-    license?: string;
-    homepage?: string;
-    repository?: string | { [key: string]: string };
-    bugs?: string | { [key: string]: string };
-    scripts?: { [key: string]: string };
+    sideEffects?: string[] | boolean;
+    scripts?: PackageJsonPredefinedScripts;
     dependencies?: { [key: string]: string };
     devDependencies?: { [key: string]: string };
     peerDependencies?: { [key: string]: string };
-    experimental?: boolean;
-    sideEffects?: string[] | boolean;
 }
