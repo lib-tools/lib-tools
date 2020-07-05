c67f5e037bbda072041c0559da5ee9da5c0e409a
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-05 12:22:48 +0630

    Update find-up.ts

--- src/utils/find-up.ts
+++ src/utils/find-up.ts
@@ -1,33 +1,36 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
 import * as path from 'path';
 
 import { pathExists } from 'fs-extra';
 
 import { isInFolder, isSamePaths } from './path-helpers';
 
-export async function findUp(
-    fileName: string | string[],
-    currentDir: string,
-    workingDir: string
-): Promise<string | null> {
-    let currentDirLocal = currentDir;
+export async function findUp(fileName: string | string[], startDir: string, endDir: string): Promise<string | null> {
+    let currentDir = startDir;
     const fileNames = Array.isArray(fileName) ? fileName : [fileName];
-    const rootPath = path.parse(currentDirLocal).root;
+    const rootPath = path.parse(currentDir).root;
 
     do {
         for (const f of fileNames) {
-            const tempPath = path.isAbsolute(f) ? f : path.resolve(currentDirLocal, f);
-            const isExsists = await pathExists(tempPath);
-            if (isExsists) {
+            const tempPath = path.isAbsolute(f) ? f : path.resolve(currentDir, f);
+            if (await pathExists(tempPath)) {
                 return tempPath;
             }
         }
 
-        if (currentDirLocal === rootPath) {
-            break;
-        }
-
-        currentDirLocal = path.dirname(currentDirLocal);
-    } while (currentDirLocal && (isSamePaths(workingDir, currentDirLocal) || isInFolder(workingDir, currentDirLocal)));
+        currentDir = path.dirname(currentDir);
+    } while (
+        currentDir &&
+        currentDir !== rootPath &&
+        (isSamePaths(endDir, currentDir) || isInFolder(endDir, currentDir))
+    );
 
     return null;
 }
