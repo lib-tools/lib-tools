36e5141c010cdfc6611970b94a2b546a5674daaf
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-05 13:41:08 +0630

    Update read-package-json.ts

--- src/helpers/read-package-json.ts
+++ src/helpers/read-package-json.ts
@@ -1,17 +1,25 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
 import { PackageJsonLike } from '../models/internals';
 
 import { readJson } from 'fs-extra';
 
-const packageJsonMap = new Map<string, PackageJsonLike>();
+const cache = new Map<string, PackageJsonLike>();
 
 export async function readPackageJson(packageJsonPath: string): Promise<PackageJsonLike> {
-    const cachedPackageJson = packageJsonMap.get(packageJsonPath);
+    const cachedPackageJson = cache.get(packageJsonPath);
     if (cachedPackageJson) {
         return cachedPackageJson;
     }
 
     const packageJson = (await readJson(packageJsonPath)) as PackageJsonLike;
-    packageJsonMap.set(packageJsonPath, packageJson);
+    cache.set(packageJsonPath, packageJson);
 
     return packageJson;
 }
