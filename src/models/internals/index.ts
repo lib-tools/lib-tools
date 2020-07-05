9051ef292b47ab97ac65a5f60103e379d9b5225c
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-05 11:29:22 +0630

    Update models

--- src/models/internals/index.ts
+++ src/models/internals/index.ts
@@ -1,5 +1,5 @@
 export * from './build-options-internal';
-export * from './lib-config-internal';
 export * from './project-build-config-internal';
 export * from './project-config-internal';
 export * from './package-jon-like';
+export * from './workflows-config-internal';
