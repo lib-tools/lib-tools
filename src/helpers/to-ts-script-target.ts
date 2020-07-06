ba284414e218ab91598e0038e74d5c444fa3a6af
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-06 18:18:58 +0630

    Update helpers

--- src/helpers/to-ts-script-target.ts
+++ src/helpers/to-ts-script-target.ts
@@ -10,36 +10,29 @@ import { ScriptTarget } from 'typescript';
 
 import { ScriptTargetString } from '../models';
 
-export function toTsScriptTarget(target?: ScriptTargetString): ScriptTarget | undefined {
-    switch (target) {
-        case 'ES5':
-        case 'es5':
-            return ScriptTarget.ES5;
-        case 'ES2015':
-        case 'es2015':
-            return ScriptTarget.ES2015;
-        case 'ES2016':
-        case 'es2016':
-            return ScriptTarget.ES2016;
-        case 'ES2017':
-        case 'es2017':
-            return ScriptTarget.ES2017;
-        case 'ES2018':
-        case 'es2018':
-            return ScriptTarget.ES2018;
-        case 'ES2019':
-        case 'es2019':
-            return ScriptTarget.ES2019;
-        case 'ES2020':
-        case 'es2020':
-            return ScriptTarget.ES2020;
-        case 'ESNext':
-        case 'esnext':
-            return ScriptTarget.ESNext;
-        case 'Latest':
-        case 'latest':
-            return ScriptTarget.Latest;
-        default:
-            return undefined;
+const esDigitRegExp = /^es(2[0-9]{3})$/i;
+
+export function toTsScriptTarget(target: ScriptTargetString): ScriptTarget | null {
+    if (target === 'es5' || target === 'ES5') {
+        return ScriptTarget.ES5;
     }
+
+    if (target === 'esnext' || target === 'ESNext') {
+        return ScriptTarget.ESNext;
+    }
+
+    if (target === 'latest' || target === 'Latest') {
+        return ScriptTarget.Latest;
+    }
+
+    // TODO: To test
+    const m = esDigitRegExp.exec(target);
+    if (m != null && m[1] != null) {
+        const digitPart = Number(m[1]);
+        if (digitPart >= 2015) {
+            return digitPart - 2013;
+        }
+    }
+
+    return null;
 }
