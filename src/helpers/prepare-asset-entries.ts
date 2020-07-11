026a92e80e08815923144acebd574a274667bd1e
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-11 11:39:07 +0630

    Update prepare-asset-entries.ts

--- src/helpers/prepare-asset-entries.ts
+++ src/helpers/prepare-asset-entries.ts
@@ -19,12 +19,30 @@ export async function prepareAssetEntries(buildAction: BuildActionInternal): Pro
             filesToCopy.push(path.relative(buildAction._projectRoot, foundReadMeFile));
         }
         const foundLicenseFile = await findUp(
-            ['LICENSE', 'LICENSE.txt'],
+            ['LICENSE', 'LICENSE.txt', 'LICENCE'],
             buildAction._projectRoot,
             buildAction._workspaceRoot
         );
         if (foundLicenseFile) {
             filesToCopy.push(path.relative(buildAction._projectRoot, foundLicenseFile));
+        }
+
+        const foundChangeLogFile = await findUp(
+            ['CHANGELOG.md', 'CHANGELOG', 'CHANGES.md', 'CHANGES', 'HISTORY.md', 'HISTORY'],
+            buildAction._projectRoot,
+            buildAction._workspaceRoot
+        );
+        if (foundChangeLogFile) {
+            filesToCopy.push(path.relative(buildAction._projectRoot, foundChangeLogFile));
+        }
+
+        const foundNoticeFile = await findUp(
+            ['NOTICE.md', 'NOTICE'],
+            buildAction._projectRoot,
+            buildAction._workspaceRoot
+        );
+        if (foundNoticeFile) {
+            filesToCopy.push(path.relative(buildAction._projectRoot, foundNoticeFile));
         }
 
         buildAction._assetEntries = filesToCopy.map((assetEntry) => {
