41a6642044f6aa5e4a81509b4a7bb5a3c9548fa0
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-11 10:51:25 +0630

    Update cli

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,8 +2,6 @@ import * as yargs from 'yargs';
 
 import { colorize } from '../utils/colorize';
 
-import { getBuildCommandModule } from './build/build-command-module';
-
 const cliPackageName = global.libCli ? global.libCli.packageName : '';
 const cliVersion = global.libCli ? global.libCli.version : '';
 const cliIsGlobal = global.libCli ? global.libCli.isGlobal : false;
@@ -11,15 +9,46 @@ const cliIsLink = global.libCli ? global.libCli.isLink : false;
 
 function initYargs(): yargs.Argv {
     const cliUsage = `${colorize(`${cliPackageName} v${cliVersion}`, 'white')}\n
-Usage:
-  lib [command] [options...]`;
+                        Usage:
+                        lib [command] [options...]`;
+    const buildCommandUsage = `${colorize(`${cliPackageName} v${cliVersion}`, 'white')}\n
+                        Usage:
+                            lib build [options...]`;
+
     const yargsInstance = yargs
         .usage(cliUsage)
-        .example('lib build', 'Build the project(s) using workflows.json configuration file')
-        .example('lib build --auto', 'Automatically detect project structure and build without configuration file')
+        .example('lib build', 'Build the project(s) using workflow.json configuration file')
+        .example('lib --workflow=auto', 'Determine and run workflow actions for the project structure automatically.')
         .example('lib --help', 'Show help')
-        .version(false)
-        .help('help')
+        .option('workflow', {
+            describe:
+                'The workflow configuration file location for set `auto` to determine and run workflow actions for the project structure automatically.',
+            type: 'string'
+        })
+        .option('env', {
+            alias: 'environment',
+            describe: 'Define the environment.'
+        })
+        .option('prod', {
+            describe: 'Shortcut flag to set environment to `production`.',
+            type: 'boolean'
+        })
+        .option('filter', {
+            describe: 'Run specific project(s) filtering by project name(s).',
+            type: 'array'
+        })
+        .option('logLevel', {
+            describe: 'Logging level for output information.',
+            type: 'string'
+        })
+        .option('verbose', {
+            describe: 'Shortcut flag to set logLevel to `debug`.',
+            type: 'boolean'
+        })
+        .option('beep', {
+            describe: 'Beep when all workflow actions completed.',
+            type: 'boolean'
+        })
         .option('h', {
             alias: 'help',
             describe: 'Show help',
@@ -31,7 +60,24 @@ Usage:
             type: 'boolean',
             global: false
         })
-        .command(getBuildCommandModule())
+        .command(
+            'build',
+            'Build the project(s)',
+            (childYargs) => {
+                return childYargs
+                    .usage(buildCommandUsage)
+                    .example('lib build', 'Build the project(s).')
+                    .option('version', {
+                        describe: 'Set the version to override the version fields of the package.json files.',
+                        type: 'string'
+                    });
+            },
+            () => {
+                // Do nothing
+            }
+        )
+        .version(false)
+        .help('help')
         .showHelpOnFail(false)
         .fail((msg, err, yi) => {
             if (err) {
@@ -76,12 +122,6 @@ export default async function (): Promise<number> {
 
         return 0;
     }
-
-    // if (command === 'help') {
-    //     yargsInstance.showHelp();
-
-    //     return 0;
-    // }
 
     return 0;
 }
