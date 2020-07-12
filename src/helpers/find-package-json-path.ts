e6af255fdbd2b3dde5432e829cdc81f20f0e5966
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-12 10:54:30 +0630

    Update find-package-json-path.ts

--- src/helpers/find-package-json-path.ts
+++ src/helpers/find-package-json-path.ts
@@ -4,30 +4,30 @@ import { pathExists } from 'fs-extra';
 
 import { findUp } from '../utils';
 
-const packageJsonPathMap = new Map<string, string>();
+const cache = new Map<string, string>();
 
 export async function findPackageJsonPath(workspaceRoot: string, projectRoot?: string): Promise<string | null> {
     if (projectRoot) {
-        const cachedPath = packageJsonPathMap.get(projectRoot);
+        const cachedPath = cache.get(projectRoot);
         if (cachedPath) {
             return cachedPath;
         }
 
         const foundPackageJsonPath = await findUp('package.json', projectRoot, workspaceRoot);
         if (foundPackageJsonPath) {
-            packageJsonPathMap.set(projectRoot, foundPackageJsonPath);
+            cache.set(projectRoot, foundPackageJsonPath);
         }
 
         return foundPackageJsonPath;
     } else {
-        const cachedPath = packageJsonPathMap.get(workspaceRoot);
+        const cachedPath = cache.get(workspaceRoot);
         if (cachedPath) {
             return cachedPath;
         }
 
         const rootPackageJsonPath = path.resolve(workspaceRoot, 'package.json');
         if (await pathExists(rootPackageJsonPath)) {
-            packageJsonPathMap.set(workspaceRoot, rootPackageJsonPath);
+            cache.set(workspaceRoot, rootPackageJsonPath);
         }
 
         return rootPackageJsonPath;
