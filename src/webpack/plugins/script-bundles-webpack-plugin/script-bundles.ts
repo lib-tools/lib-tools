507a9dded541f3ce9883cabda040e11960e72adf
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-15 08:49:13 +0630

    Update script-bundles.ts

--- src/webpack/plugins/script-bundles-webpack-plugin/script-bundles.ts
+++ src/webpack/plugins/script-bundles-webpack-plugin/script-bundles.ts
@@ -11,9 +11,6 @@ export async function performScriptBundles(buildAction: BuildActionInternal, log
         return;
     }
 
-    const scriptOptions = buildAction.script || {};
-    const sourceMap = scriptOptions.sourceMap !== false ? true : false;
-
     for (const bundleOptions of buildAction._script._bundles) {
         const rollupOptions = getRollupConfig(bundleOptions, buildAction, logger);
 
@@ -25,9 +22,9 @@ export async function performScriptBundles(buildAction: BuildActionInternal, log
         if (bundleOptions.minify) {
             const minFilePath = bundleOptions._outputFilePath.replace(/\.js$/i, '.min.js');
 
-            logger.debug(`Minifying ${path.basename(bundleOptions._outputFilePath)}`);
+            logger.debug(`Writing minify file ${path.basename(minFilePath)}`);
 
-            await minifyJsBundle(bundleOptions._outputFilePath, minFilePath, sourceMap, logger);
+            await minifyJsBundle(bundleOptions._outputFilePath, minFilePath, bundleOptions.sourceMap, logger);
         }
     }
 }
