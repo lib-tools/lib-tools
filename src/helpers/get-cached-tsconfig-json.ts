22ed58f44ddd46ef55cbb1fb541f07e87b275cf6
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-18 12:22:17 +0630

    Update get-cached-tsconfig-json.ts

---
+++ src/helpers/get-cached-tsconfig-json.ts
@@ -0,0 +1,25 @@
+import * as ts from 'typescript';
+
+import { formatTsDiagnostics } from './format-ts-diagnostics';
+
+const cache = new Map<string, { [key: string]: unknown }>();
+
+export function getCachedTsconfigJson(tsConfigPath: string): { [key: string]: unknown } {
+    const cachedTsConfigJson = cache.get(tsConfigPath);
+    if (cachedTsConfigJson) {
+        return cachedTsConfigJson;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    const jsonConfigFile = ts.readConfigFile(tsConfigPath, ts.sys.readFile);
+    if (jsonConfigFile.error && jsonConfigFile.error.length) {
+        const formattedMsg = formatTsDiagnostics(jsonConfigFile.error);
+        if (formattedMsg) {
+            throw new Error(formattedMsg);
+        }
+    }
+
+    cache.set(tsConfigPath, jsonConfigFile.config);
+
+    return jsonConfigFile.config as { [key: string]: unknown };
+}
