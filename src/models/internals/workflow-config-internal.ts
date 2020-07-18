219323fabd3eff61305e2aff19fc7a6b7d9c6463
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-18 12:21:08 +0630

    Update workflow-config-internal.ts

--- src/models/internals/workflow-config-internal.ts
+++ src/models/internals/workflow-config-internal.ts
@@ -3,9 +3,6 @@ import { WorkflowConfig } from '../workflow-config';
 import { ProjectConfigInternal } from './project-config-internal';
 
 export interface WorkflowConfigInternal extends WorkflowConfig {
-    _workspaceRoot: string;
-    _auto: boolean;
-    _configPath: string | null;
     projects: {
         [key: string]: ProjectConfigInternal;
     };
