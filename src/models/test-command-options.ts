81d6a6039e11d8e6b8cf82b965ccc53b59a4ee3a
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-29 23:56:55 +0630

    Update test-command-options.ts

--- src/models/test-command-options.ts
+++ src/models/test-command-options.ts
@@ -1,11 +1,20 @@
 import { SharedCommandOptions } from './shared-command-options';
 
-/**
- * @additionalProperties true
- */
 export interface TestCommandOptions extends SharedCommandOptions {
+    /**
+     * A list of browsers to launch and capture.
+     */
     browsers?: string | string[];
+    /**
+     * A list of reporters to use.
+     */
     reporters?: string | string[];
+    /**
+     * Output code coverage report.
+     */
     codeCoverage?: boolean;
+    /**
+     * Custom karma.conf.js file path.
+     */
     karmaConfig?: string;
 }
