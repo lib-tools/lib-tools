8c28859212b4180d4c63756df452496b8045a278
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-29 20:29:03 +0630

    Update karma-plugin

--- src/karma-plugin/index.ts
+++ src/karma-plugin/index.ts
@@ -11,8 +11,6 @@ import { LoggerBase } from '../utils';
 
 export interface PluginOptions extends KarmaConfigOptions {
     webpackConfig: webpack.Configuration;
-    webpack?: webpack.Configuration;
-    webpackMiddleware?: webpackDevMiddleware.Options;
     codeCoverage?: boolean;
     logger: LoggerBase;
 }
@@ -23,10 +21,7 @@ export type NextHandleFunction = (req: any, res: http.ServerResponse, next: (err
 let blocked: (() => void)[] = [];
 let isBlocked = false;
 let webpackMiddleware: webpackDevMiddleware.WebpackDevMiddleware & NextHandleFunction;
-// let successCb: () => void;
-// let failureCb: () => void;
 
-// Block requests until the Webpack compilation is done.
 function requestBlocker() {
     return (_1: { url: string }, _2: http.ServerResponse, next: () => void) => {
         if (isBlocked) {
@@ -37,7 +32,6 @@ function requestBlocker() {
     };
 }
 
-// When a request is not found in the karma server, try looking for it from the webpack server root.
 function fallbackMiddleware() {
     return (req: { url: string }, res: http.ServerResponse, next: () => void) => {
         if (webpackMiddleware) {
@@ -68,12 +62,10 @@ const init = (
 
     const webpackConfig = config.webpackConfig;
     const webpackMiddlewareConfig: webpackDevMiddleware.Options = {
-        // Hide webpack output because its noisy.
-        // logLevel: 'error',
+        logLevel: 'error',
         stats: {
             colors: config.colors
         },
-        // watchOptions: { poll: config.poll },
         publicPath: '/_karma_webpack_/'
     };
 
@@ -84,29 +76,17 @@ const init = (
     }
 
     const compilationErrorCb = (_: string | undefined, errors: string[]) => {
-        // Notify potential listeners of the compile error
         emitter.emit('compile_error', errors);
-
-        // Finish Karma run early in case of compilation error.
         emitter.emit('run_complete', [], { exitCode: 1 });
-
-        // Unblock any karma requests (potentially started using `karma run`)
         unblock();
     };
 
     webpackConfig.plugins = webpackConfig.plugins || [];
     webpackConfig.plugins.push(new FailureKarmaWebpackPlugin(compilationErrorCb));
 
-    // Use existing config if any.
-    config.webpack = Object.assign(webpackConfig, config.webpack);
-    config.webpackMiddleware = Object.assign(webpackMiddlewareConfig, config.webpackMiddleware);
-
-    // Our custom context and debug files list the webpack bundles directly instead of using
-    // the karma files array.
     config.customContextFile = `${__dirname}/karma-context.html`;
     config.customDebugFile = `${__dirname}/karma-debug.html`;
 
-    // Add the request blocker and the webpack server fallback.
     config.beforeMiddleware = config.beforeMiddleware || [];
     config.beforeMiddleware.push('lib-tools--blocker');
     config.middleware = config.middleware || [];
@@ -117,7 +97,6 @@ const init = (
         webpackConfig.plugins.unshift(new TurnOffWatchWebpackPlugin());
     }
 
-    // Files need to be served from a custom path for Karma.
     webpackConfig.output = webpackConfig.output || {};
     webpackConfig.output.path = '/_karma_webpack_/';
     webpackConfig.output.publicPath = '/_karma_webpack_/';
@@ -151,12 +130,7 @@ const init = (
         if (stats.compilation.errors.length > 0) {
             logger.error(stats.toString('errors-only'));
             lastCompilationHash = undefined;
-            // Emit a failure build event if there are compilation errors.
-            // if (failureCb) {
-            //     failureCb();
-            // }
         } else if (stats.hash !== lastCompilationHash) {
-            // Refresh karma only when there are no webpack errors, and if the compilation changed.
             lastCompilationHash = stats.hash;
             emitter.refreshFiles();
         }
@@ -164,10 +138,8 @@ const init = (
         unblock();
     });
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
     webpackMiddleware = webpackDevMiddleware(compiler, webpackMiddlewareConfig);
 
-    // Forward requests to webpack server.
     customFileHandlers.push({
         urlRegex: /^\/_karma_webpack_\/.*/,
         handler: (req: { url: string }, res: http.ServerResponse) => {
@@ -175,9 +147,9 @@ const init = (
                 const alwaysServe = [
                     '/_karma_webpack_/runtime.js',
                     '/_karma_webpack_/polyfills.js',
-                    '/_karma_webpack_/polyfills-es5.js'
+                    '/_karma_webpack_/vendor.js'
                 ];
-                if (alwaysServe.indexOf(req.url) !== -1) {
+                if (alwaysServe.includes(req.url)) {
                     res.statusCode = 200;
                     res.end();
                 } else {
