db29d450ea455799d47d80d93ecdfca27012c59e
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-29 13:48:36 +0630

    Update prepare-asset-entries.ts

--- src/helpers/prepare-asset-entries.ts
+++ src/helpers/prepare-asset-entries.ts
@@ -1,51 +1,51 @@
 import * as path from 'path';
 
-import { BuildActionInternal } from '../models/internals';
+import { BuildConfigInternal } from '../models';
 import { findUp } from '../utils';
 
-export async function prepareAssetEntries(buildAction: BuildActionInternal): Promise<void> {
-    if (buildAction.copy) {
-        buildAction._assetEntries = buildAction.copy.map((assetEntry) =>
+export async function prepareAssetEntries(buildConfig: BuildConfigInternal): Promise<void> {
+    if (buildConfig.copy) {
+        buildConfig._assetEntries = buildConfig.copy.map((assetEntry) =>
             typeof assetEntry === 'string' ? { from: assetEntry } : { ...assetEntry }
         );
     } else {
         const filesToCopy: string[] = [];
         const foundReadMeFile = await findUp(
             ['README.md', 'README'],
-            buildAction._projectRoot,
-            buildAction._workspaceRoot
+            buildConfig._projectRoot,
+            buildConfig._workspaceRoot
         );
         if (foundReadMeFile) {
-            filesToCopy.push(path.relative(buildAction._projectRoot, foundReadMeFile));
+            filesToCopy.push(path.relative(buildConfig._projectRoot, foundReadMeFile));
         }
         const foundLicenseFile = await findUp(
             ['LICENSE', 'LICENSE.txt', 'LICENCE'],
-            buildAction._projectRoot,
-            buildAction._workspaceRoot
+            buildConfig._projectRoot,
+            buildConfig._workspaceRoot
         );
         if (foundLicenseFile) {
-            filesToCopy.push(path.relative(buildAction._projectRoot, foundLicenseFile));
+            filesToCopy.push(path.relative(buildConfig._projectRoot, foundLicenseFile));
         }
 
         const foundChangeLogFile = await findUp(
             ['CHANGELOG.md', 'CHANGELOG', 'CHANGES.md', 'CHANGES', 'HISTORY.md', 'HISTORY'],
-            buildAction._projectRoot,
-            buildAction._workspaceRoot
+            buildConfig._projectRoot,
+            buildConfig._workspaceRoot
         );
         if (foundChangeLogFile) {
-            filesToCopy.push(path.relative(buildAction._projectRoot, foundChangeLogFile));
+            filesToCopy.push(path.relative(buildConfig._projectRoot, foundChangeLogFile));
         }
 
         const foundNoticeFile = await findUp(
             ['NOTICE.md', 'NOTICE'],
-            buildAction._projectRoot,
-            buildAction._workspaceRoot
+            buildConfig._projectRoot,
+            buildConfig._workspaceRoot
         );
         if (foundNoticeFile) {
-            filesToCopy.push(path.relative(buildAction._projectRoot, foundNoticeFile));
+            filesToCopy.push(path.relative(buildConfig._projectRoot, foundNoticeFile));
         }
 
-        buildAction._assetEntries = filesToCopy.map((assetEntry) => {
+        buildConfig._assetEntries = filesToCopy.map((assetEntry) => {
             return {
                 from: assetEntry
             };
