bdab4d31a6ee1be695cbed97bf99711733f9bc91
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-29 13:39:05 +0630

    Update apply-env-overrides.ts

--- src/helpers/apply-env-overrides.ts
+++ src/helpers/apply-env-overrides.ts
@@ -1,10 +1,10 @@
-import { OverridableAction } from '../models';
+import { OverridableConfig } from '../models';
 
 export function applyEnvOverrides<TConfigBase>(
-    overridableAction: OverridableAction<TConfigBase>,
+    overridableConfig: OverridableConfig<TConfigBase>,
     env: { [key: string]: boolean | string }
 ): void {
-    if (!overridableAction.envOverrides || !Object.keys(overridableAction.envOverrides).length) {
+    if (!overridableConfig.envOverrides || !Object.keys(overridableConfig.envOverrides).length) {
         return;
     }
 
@@ -36,15 +36,15 @@ export function applyEnvOverrides<TConfigBase>(
             buildTargets.push(key);
         });
 
-    Object.keys(overridableAction.envOverrides).forEach((buildTargetKey: string) => {
+    Object.keys(overridableConfig.envOverrides).forEach((buildTargetKey: string) => {
         const targetName = buildTargetKey;
         const targets = targetName.split(',');
         targets.forEach((t) => {
             t = t.trim();
-            if (buildTargets.indexOf(t) > -1 && overridableAction.envOverrides) {
-                const newConfig = overridableAction.envOverrides[t];
+            if (buildTargets.indexOf(t) > -1 && overridableConfig.envOverrides) {
+                const newConfig = overridableConfig.envOverrides[t];
                 if (newConfig && typeof newConfig === 'object') {
-                    overrideActionConfig(overridableAction as { [key: string]: unknown }, newConfig);
+                    overrideActionConfig(overridableConfig as { [key: string]: unknown }, newConfig);
                 }
             }
         });
