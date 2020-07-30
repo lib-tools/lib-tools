ea6c44f809152b4a7b83392d204a386df223a7c1
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-30 11:12:06 +0630

    Update helpers

--- src/helpers/detect-tsconfig-path.ts
+++
@@ -1,9 +0,0 @@
-import { findUp } from '../utils';
-
-export async function detectTsconfigPath(workspaceRoot: string, projectRoot: string): Promise<string | null> {
-    return findUp(
-        ['tsconfig.build.json', 'tsconfig-build.json', 'tsconfig.lib.json', 'tsconfig-lib.json', 'tsconfig.json'],
-        projectRoot,
-        workspaceRoot
-    );
-}
