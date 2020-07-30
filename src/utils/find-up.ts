da7644a5be15b5c3b7f4222d888d25f5a57d880e
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-30 11:09:52 +0630

    Update find-up.ts

--- src/utils/find-up.ts
+++ src/utils/find-up.ts
@@ -4,25 +4,32 @@ import { pathExists } from 'fs-extra';
 
 import { isInFolder, isSamePaths } from './path-helpers';
 
-export async function findUp(pathName: string | string[], startDir: string, endDir: string): Promise<string | null> {
-    let currentDir = startDir;
-    const pathNames = Array.isArray(pathName) ? pathName : [pathName];
-    const rootPath = path.parse(currentDir).root;
+export async function findUp(
+    pathName: string | string[],
+    startDirs: string | string[],
+    endDir: string
+): Promise<string | null> {
+    const startDirsArray = Array.isArray(startDirs) ? startDirs : [startDirs];
+    for (const startDir of startDirsArray) {
+        let currentDir = startDir;
+        const pathNames = Array.isArray(pathName) ? pathName : [pathName];
+        const rootPath = path.parse(currentDir).root;
 
-    do {
-        for (const p of pathNames) {
-            const tempPath = path.isAbsolute(p) ? p : path.resolve(currentDir, p);
-            if (await pathExists(tempPath)) {
-                return tempPath;
+        do {
+            for (const p of pathNames) {
+                const tempPath = path.isAbsolute(p) ? p : path.resolve(currentDir, p);
+                if (await pathExists(tempPath)) {
+                    return tempPath;
+                }
             }
-        }
 
-        currentDir = path.dirname(currentDir);
-    } while (
-        currentDir &&
-        currentDir !== rootPath &&
-        (isSamePaths(endDir, currentDir) || isInFolder(endDir, currentDir))
-    );
+            currentDir = path.dirname(currentDir);
+        } while (
+            currentDir &&
+            currentDir !== rootPath &&
+            (isSamePaths(endDir, currentDir) || isInFolder(endDir, currentDir))
+        );
+    }
 
     return null;
 }
