c355d7d3aa7e50521c2db0b663010dd52152de77
Moe Myat Zaw <mmzliveid@gmail.com>
2020-07-31 11:29:25 +0630

    Update test-config-internal.ts

--- src/models/test-config-internal.ts
+++ src/models/test-config-internal.ts
@@ -6,7 +6,8 @@ export interface TestConfigInternal extends TestConfig {
     _projectName: string;
     _projectRoot: string;
 
-    _entryFilePath: string | null;
-    _tsConfigPath: string | null;
-    _karmaConfigPath: string | null;
+    _entryFilePath?: string | null;
+    _tsConfigPath?: string | null;
+    _karmaConfigPath?: string | null;
+    _codeCoverage?: boolean;
 }
