6e4f202ac19c61da3c824ad456935fdb2540f4e6
Moe Myat Zaw <mmzliveid@gmail.com>
2020-08-02 20:21:01 +0630

    Update models

--- src/models/test-config-internal.ts
+++ src/models/test-config-internal.ts
@@ -1,10 +1,13 @@
 import { TestConfig } from './test-config';
 
+import { PackageJsonLike } from './package-jon-like';
+
 export interface TestConfigInternal extends TestConfig {
     _workspaceRoot: string;
     _config: 'auto' | string;
     _projectName: string;
     _projectRoot: string;
+    _packageJson: PackageJsonLike | null;
 
     _entryFilePath?: string | null;
     _tsConfigPath?: string | null;
