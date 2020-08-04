c3dc451820e7746464d55a1e5152f36f222af5da
Moe Myat Zaw <mmzliveid@gmail.com>
2020-08-04 19:29:16 +0630

    Update test-config.ts

--- src/models/test-config.ts
+++ src/models/test-config.ts
@@ -4,19 +4,50 @@ import { OverridableConfig } from './overridable-config';
  * @additionalProperties false
  */
 export interface TestConfigBase {
-    entry?: string;
-    tsConfig?: string;
+    /**
+     * Index file for test.
+     */
+    testIndexFile?: string;
+
+    /**
+     * Polyfill entries.
+     */
     polyfills?: string | string[];
+
+    /**
+     * Typescript configuration file.
+     */
+    tsConfig?: string;
+
+    /**
+     * Karma configuration file.
+     */
     karmaConfig?: string;
+
+    /**
+     * A list of browsers to launch and capture.
+     */
     browsers?: string[];
+
+    /**
+     * A list of reporters to use.
+     */
     reporters?: string[];
+
+    /**
+     * Output code coverage report.
+     */
+    codeCoverage?: boolean;
+
+    /**
+     * A list of minimatch pattern to exclude files from code coverage report.
+     */
+    codeCoverageExclude?: string[];
+
     /**
      * If true, extract and include source maps from existing vendor module source map files.
      */
     vendorSourceMap?: boolean;
-    sourceMap?: boolean;
-    codeCoverage?: boolean;
-    codeCoverageExclude?: string[];
 
     /**
      * Set true to skip the task.
