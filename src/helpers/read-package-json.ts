89956c42fff861e9f3c3aa77be125e94e0ecf827
Moe Myat Zaw <mmzliveid@gmail.com>
2020-08-04 19:55:56 +0630

    Add read-package-json.ts

---
+++ src/helpers/read-package-json.ts
@@ -0,0 +1,17 @@
+import { PackageJsonLike } from '../models';
+
+import { readJson } from 'fs-extra';
+
+const cache = new Map<string, PackageJsonLike>();
+
+export async function readPackageJson(packageJsonPath: string): Promise<PackageJsonLike> {
+    const cachedPackageJson = cache.get(packageJsonPath);
+    if (cachedPackageJson) {
+        return cachedPackageJson;
+    }
+
+    const packageJson = (await readJson(packageJsonPath)) as PackageJsonLike;
+    cache.set(packageJsonPath, packageJson);
+
+    return packageJson;
+}
