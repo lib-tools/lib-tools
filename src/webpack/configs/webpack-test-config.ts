fa6387202c150a3e085ee505a4055024821604f0
Moe Myat Zaw <mmzliveid@gmail.com>
2020-08-04 20:00:45 +0630

    Update webpack-test-config.ts

--- src/webpack/configs/webpack-test-config.ts
+++ src/webpack/configs/webpack-test-config.ts
@@ -2,53 +2,16 @@ import * as path from 'path';
 import { promisify } from 'util';
 
 import { AngularCompilerPlugin, NgToolsLoader } from '@ngtools/webpack';
-import * as Ajv from 'ajv';
 import { pathExists } from 'fs-extra';
 import * as glob from 'glob';
-import { ConfigOptions as KarmaConfigOptions } from 'karma';
 import { Configuration, Plugin, Rule } from 'webpack';
 
-import {
-    applyProjectExtends,
-    findPackageJsonPath,
-    findTestEntryFile,
-    findTestTsconfigFile,
-    getCachedPackageJson,
-    getCachedWorkflowConfigSchema,
-    isAngularProject
-} from '../../helpers';
-import {
-    PackageJsonLike,
-    ProjectConfigInternal,
-    TestCommandOptions,
-    TestConfigInternal,
-    WorkflowConfig
-} from '../../models';
-import { LogLevelString, findUp, isInFolder, isSamePaths, readJsonWithComments } from '../../utils';
+import { isAngularProject } from '../../helpers';
+import { TestCommandOptions, TestConfigInternal } from '../../models';
 
 import { TestInfoWebpackPlugin } from '../plugins/test-info-webpack-plugin';
 
 const globAsync = promisify(glob);
-const ajv = new Ajv();
-
-export async function getWebpackTestConfigFromKarma(
-    karmaConfig: KarmaConfigOptions & { configFile: string }
-): Promise<Configuration> {
-    const karmaConfigFile = karmaConfig.configFile;
-    const testConfig = await getTestConfigFromKarmaConfigFile(karmaConfigFile);
-    if (!testConfig) {
-        throw new Error('Could not detect workflow test config.');
-    }
-
-    const logLevel = karmaConfig.logLevel != null ? (karmaConfig.logLevel as LogLevelString) : 'info';
-    const argv = {
-        logLevel
-    };
-
-    const webpackConfig = await getWebpackTestConfig(testConfig, argv);
-
-    return webpackConfig;
-}
 
 export async function getWebpackTestConfig(
     testConfig: TestConfigInternal,
@@ -73,18 +36,17 @@ export async function getWebpackTestConfig(
     if (testConfig._tsConfigPath) {
         const tsConfigPath = testConfig._tsConfigPath;
         if (
-            testConfig._entryFilePath &&
-            /\.tsx?$/i.test(testConfig._entryFilePath) &&
+            testConfig._testIndexFilePath &&
+            /\.tsx?$/i.test(testConfig._testIndexFilePath) &&
             (await isAngularProject(testConfig._workspaceRoot, testConfig._packageJson))
         ) {
             rules.push({
                 test: /\.tsx?$/,
                 loader: NgToolsLoader,
                 options: {
-                    mainPath: testConfig._entryFilePath,
+                    mainPath: testConfig._testIndexFilePath,
                     configFile: tsConfigPath,
                     skipCodeGeneration: true,
-                    sourceMap: testConfig.sourceMap,
                     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
                     contextElementDependencyConstructor: require('webpack/lib/dependencies/ContextElementDependency'),
                     directTemplateLoading: true
@@ -107,13 +69,28 @@ export async function getWebpackTestConfig(
         }
     }
 
-    if (testConfig.codeCoverage) {
+    const codeCoverage =
+        testCommandOptions.codeCoverage != null ? testCommandOptions.codeCoverage : testConfig.codeCoverage;
+    if (codeCoverage) {
         const exclude: (string | RegExp)[] = [/\.(e2e|spec)\.tsx?$/, /node_modules/];
-        const codeCoverageExclude = testConfig.codeCoverageExclude;
 
-        if (codeCoverageExclude) {
-            for (const excludeGlob of codeCoverageExclude) {
-                const excludeFiles = await globAsync(path.join(testConfig._projectRoot, excludeGlob), { nodir: true });
+        const codeCoverageExcludes =
+            testCommandOptions.codeCoverageExclude != null
+                ? Array.isArray(testCommandOptions.codeCoverageExclude)
+                    ? testCommandOptions.codeCoverageExclude
+                    : testCommandOptions.codeCoverageExclude.split(',')
+                : testConfig.codeCoverageExclude;
+
+        if (codeCoverageExcludes) {
+            for (const excludePattern of codeCoverageExcludes) {
+                if (!excludePattern) {
+                    continue;
+                }
+
+                const excludeFiles = await globAsync(path.join(testConfig._projectRoot, excludePattern), {
+                    nodir: true
+                });
+
                 for (const f of excludeFiles) {
                     const nf = path.normalize(f);
                     exclude.push(nf);
@@ -136,8 +113,8 @@ export async function getWebpackTestConfig(
     const webpackConfig: Configuration = {
         name: testConfig._projectName,
         mode: 'development',
-        // devtool: testConfig.sourceMap ? false : 'eval',
-        devtool: testConfig.sourceMap ? 'inline-source-map' : 'eval',
+        // 'inline-source-map'
+        devtool: 'eval',
         resolve: {
             extensions: ['.ts', '.tsx', '.js', '.mjs'],
             mainFields: ['es2017', 'es2015', 'browser', 'module', 'main']
@@ -145,8 +122,6 @@ export async function getWebpackTestConfig(
         output: {
             path: '/_karma_webpack_/',
             publicPath: '/_karma_webpack_/',
-            // Enforce that the output filename is dynamic and doesn't contain chunkhashes
-            // TODO: To review
             filename: '[name].js',
             crossOriginLoading: 'anonymous'
         },
@@ -156,12 +131,10 @@ export async function getWebpackTestConfig(
         },
         plugins,
         optimization: {
-            // runtimeChunk: false,
-            // splitChunks: false,
             runtimeChunk: 'single',
             splitChunks: {
                 maxAsyncRequests: Infinity,
-                chunks: (chunk: { name: string }) => !isPolyfillsEntry(chunk.name),
+                chunks: (chunk: { name: string }) => chunk.name !== 'polyfills',
                 cacheGroups: {
                     vendors: false,
                     vendor: {
@@ -172,7 +145,7 @@ export async function getWebpackTestConfig(
 
                             return (
                                 /[\\/]node_modules[\\/]/.test(moduleName) &&
-                                !chunks.some(({ name }) => isPolyfillsEntry(name))
+                                !chunks.some(({ name }) => name === 'polyfills')
                             );
                         }
                     }
@@ -182,11 +155,12 @@ export async function getWebpackTestConfig(
         stats: 'errors-only'
     };
 
-    if (testConfig._entryFilePath || (testConfig.polyfills && testConfig.polyfills.length > 0)) {
+    if (testConfig._testIndexFilePath || (testConfig.polyfills && testConfig.polyfills.length > 0)) {
         webpackConfig.entry = {};
-        if (testConfig._entryFilePath) {
-            webpackConfig.entry.main = testConfig._entryFilePath;
+        if (testConfig._testIndexFilePath) {
+            webpackConfig.entry.main = testConfig._testIndexFilePath;
         }
+
         if (testConfig.polyfills && testConfig.polyfills.length > 0) {
             const polyfills = Array.isArray(testConfig.polyfills) ? testConfig.polyfills : [testConfig.polyfills];
             webpackConfig.entry.polyfills = await resolvePolyfillPaths(polyfills, testConfig._projectRoot);
@@ -198,10 +172,6 @@ export async function getWebpackTestConfig(
     }
 
     return webpackConfig;
-}
-
-function isPolyfillsEntry(name: string) {
-    return name === 'polyfills' || name === 'polyfills-es5';
 }
 
 async function resolvePolyfillPaths(polyfills: string[], projectRoot: string): Promise<string[]> {
@@ -217,145 +187,4 @@ async function resolvePolyfillPaths(polyfills: string[], projectRoot: string): P
     }
 
     return resolvedPaths;
-}
-
-async function getTestConfigFromKarmaConfigFile(karmaConfigFile: string): Promise<TestConfigInternal | null> {
-    const karmaConfigBaseDir = path.dirname(karmaConfigFile);
-    const foundWorkflowConfigPath = await findUp(
-        ['workflow.json'],
-        karmaConfigBaseDir,
-        path.parse(karmaConfigBaseDir).root
-    );
-
-    if (foundWorkflowConfigPath) {
-        const workflowConfig = (await readJsonWithComments(foundWorkflowConfigPath)) as WorkflowConfig;
-        const schema = await getCachedWorkflowConfigSchema();
-        if (!ajv.getSchema('workflowSchema')) {
-            ajv.addSchema(schema, 'workflowSchema');
-        }
-        const valid = ajv.validate('workflowSchema', workflowConfig);
-        if (!valid) {
-            throw new Error(`Invalid workflow configuration. ${ajv.errorsText()}`);
-        }
-
-        const workspaceRoot = path.dirname(foundWorkflowConfigPath);
-        const projectConfigs = Object.keys(workflowConfig.projects).map((projectName) => {
-            const projectConfig = workflowConfig.projects[projectName];
-
-            if (projectConfig.root && path.isAbsolute(projectConfig.root)) {
-                throw new Error(
-                    `Invalid workflow configuration. The 'projects[${projectName}].root' must be relative path.`
-                );
-            }
-
-            const projectRoot = path.resolve(workspaceRoot, projectConfig.root || '');
-
-            const projectConfigInternal: ProjectConfigInternal = {
-                ...projectConfig,
-                _workspaceRoot: workspaceRoot,
-                _config: foundWorkflowConfigPath,
-                _projectName: projectName,
-                _projectRoot: projectRoot
-            };
-
-            return projectConfigInternal;
-        });
-
-        for (const projectConfig of projectConfigs) {
-            await applyProjectExtends(projectConfig, projectConfigs, projectConfig._config);
-            if (!projectConfig.tasks || !projectConfig.tasks.test) {
-                continue;
-            }
-
-            const testConfig = projectConfig.tasks.test;
-
-            if (testConfig.skip) {
-                continue;
-            }
-
-            const projectRoot = projectConfig._projectRoot;
-
-            if (
-                testConfig.karmaConfig &&
-                !isSamePaths(karmaConfigFile, path.resolve(projectRoot, testConfig.karmaConfig))
-            ) {
-                continue;
-            }
-
-            let tsConfigPath: string | null = null;
-            let entryFilePath: string | null = null;
-
-            if (testConfig.tsConfig) {
-                tsConfigPath = path.resolve(projectRoot, testConfig.tsConfig);
-            } else {
-                tsConfigPath = await findTestTsconfigFile(projectRoot, workspaceRoot);
-            }
-
-            if (testConfig.entry) {
-                entryFilePath = path.resolve(projectRoot, testConfig.entry);
-            } else {
-                entryFilePath = await findTestEntryFile(projectRoot, workspaceRoot, tsConfigPath);
-            }
-
-            let packageJson: PackageJsonLike | null = null;
-
-            const packageJsonPath = await findPackageJsonPath(projectRoot, workspaceRoot);
-            if (packageJsonPath) {
-                packageJson = await getCachedPackageJson(packageJsonPath);
-            }
-
-            const testConfigInternal: TestConfigInternal = {
-                ...testConfig,
-                _config: projectConfig._config,
-                _workspaceRoot: workspaceRoot,
-                _projectRoot: projectRoot,
-                _projectName: projectConfig._projectName,
-                _packageJson: packageJson,
-                _entryFilePath: entryFilePath,
-                _tsConfigPath: tsConfigPath,
-                _karmaConfigPath: karmaConfigFile
-            };
-
-            return testConfigInternal;
-        }
-
-        return null;
-    } else {
-        const workspaceRoot = isInFolder(process.cwd(), karmaConfigBaseDir) ? process.cwd() : karmaConfigBaseDir;
-        const tsConfigPath = await findTestTsconfigFile(karmaConfigBaseDir, workspaceRoot);
-        const entryFilePath = await findTestEntryFile(karmaConfigBaseDir, workspaceRoot, tsConfigPath);
-        if (!entryFilePath) {
-            return null;
-        }
-
-        const packageJsonPath = await findUp('package.json', karmaConfigBaseDir, workspaceRoot);
-        let packageJson: PackageJsonLike | null = null;
-        if (!packageJsonPath) {
-            return null;
-        }
-
-        packageJson = await getCachedPackageJson(packageJsonPath);
-        const packageName = packageJson.name;
-        if (!packageName) {
-            return null;
-        }
-
-        let packageNameWithoutScope = packageName;
-        const slashIndex = packageName.indexOf('/');
-        if (slashIndex > -1 && packageName.startsWith('@')) {
-            packageNameWithoutScope = packageName.substr(slashIndex + 1);
-        }
-        const projectName = packageNameWithoutScope.replace(/\//g, '-');
-
-        return {
-            _config: 'auto',
-            _workspaceRoot: workspaceRoot,
-            _projectRoot: karmaConfigBaseDir,
-            _projectName: projectName,
-            _packageJson: packageJson,
-            _tsConfigPath: tsConfigPath,
-            _entryFilePath: entryFilePath,
-            _karmaConfigPath: karmaConfigFile
-        };
-    }
 }
