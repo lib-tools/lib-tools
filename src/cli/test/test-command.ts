ca7005cfe40be2a89300dc5012b007359cd0a5a6
Moe Myat Zaw <mmzliveid@gmail.com>
2020-08-04 19:15:13 +0630

    Add test-command.ts

---
+++ src/cli/test/test-command.ts
@@ -0,0 +1,74 @@
+import * as yargs from 'yargs';
+
+import { colorize } from '../../utils/colorize';
+
+export function getTestCommand(cliPackageName: string, cliVersion: string): yargs.CommandModule {
+    const testCommandUsage = `${colorize(`${cliPackageName} v${cliVersion}`, 'white')}\n
+    Usage:
+        lib test [options...]`;
+
+    return {
+        command: 'test',
+        describe: 'Test the project(s)',
+        builder: (childYargs) =>
+            childYargs
+                .usage(testCommandUsage)
+                .example('lib test', 'Test the project(s).')
+                .option('browsers', {
+                    describe: 'A list of browsers to launch and capture.',
+                    type: 'array'
+                })
+                .option('reporters', {
+                    describe: 'A list of reporters to use.',
+                    type: 'array'
+                })
+                .option('codeCoverage', {
+                    describe: 'Output code coverage report.',
+                    type: 'boolean'
+                })
+                .option('karmaConfig', {
+                    describe: 'Custom karma.conf.js file path.',
+                    type: 'string'
+                })
+                // Shared command options
+                .option('workflow', {
+                    describe:
+                        'The workflow configuration file location or `auto` to analyze project structure and run test automatically.',
+                    type: 'string'
+                })
+                .option('env', {
+                    alias: 'environment',
+                    describe:
+                        'Environment name to override the task configuration with `envOverrides[environment]` options.'
+                })
+                .option('prod', {
+                    describe: 'Shortcut flag to set environment to `production`.',
+                    type: 'boolean'
+                })
+                .option('filter', {
+                    describe: 'Filter the project(s) by project name(s).',
+                    type: 'array'
+                })
+                .option('logLevel', {
+                    describe: 'Logging level for output information.',
+                    // type: 'string'
+                    choices: ['debug', 'info', 'warn', 'error', 'disable']
+                })
+                .option('verbose', {
+                    describe: 'Shortcut flag to set logLevel to `debug`.',
+                    type: 'boolean'
+                })
+                .option('watch', {
+                    describe: 'Run in watch mode.',
+                    type: 'boolean'
+                })
+                .option('h', {
+                    alias: 'help',
+                    describe: 'Show help',
+                    type: 'boolean'
+                }),
+        handler: () => {
+            // Do nothing
+        }
+    };
+}
