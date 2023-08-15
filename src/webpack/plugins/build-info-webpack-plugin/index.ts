454f25693441cb27e94dba0c88bfa9a869383ebe
Moe Myat Zaw <mmzliveid@gmail.com>
2023-08-15 18:25:20 +0630

    Update build-info-webpack-plugin

--- src/webpack/plugins/build-info-webpack-plugin/index.ts
+++ src/webpack/plugins/build-info-webpack-plugin/index.ts
@@ -3,12 +3,6 @@ import chalk from 'chalk';
 import { BuildConfigInternal } from '../../../models/index.js';
 import { LogLevelString, Logger } from '../../../utils/index.js';
 
-if (!global.buildCounter) {
-    global.buildCounter = { count: 0 };
-}
-
-const buildCounter = global.buildCounter || { count: 0 };
-
 export interface BuildInfoWebpackPluginOptions {
     buildConfig: BuildConfigInternal;
     logLevel?: LogLevelString;
@@ -28,10 +22,6 @@ export class BuildInfoWebpackPlugin {
     }
 
     apply(): void {
-        if (buildCounter.count > 0) {
-            this.logger.info('\n');
-        }
-        ++buildCounter.count;
         this.logger.info(`Running build for ${chalk.cyan(this.options.buildConfig._projectName)}`);
     }
 }
